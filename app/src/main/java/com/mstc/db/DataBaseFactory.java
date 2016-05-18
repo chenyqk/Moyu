7f403cb38c973af36b413cb04bf7581167f9a5df
astroromance <chen.yuqing@mail.scut.edu.cn>
2016-05-18 14:50:58 +0800

    add some update function in DataBase Factory

--- app/src/main/java/com/mstc/db/DataBaseFactory.java
+++ app/src/main/java/com/mstc/db/DataBaseFactory.java
@@ -114,4 +114,96 @@ public final class DataBaseFactory {
 
         db.delete(MoyuContract.DeletedRepeatAffairEntry.TABLE_NAME,selection,selectionArgs);
     }
+
+    public static int UpdateAffair(final DataBaseHelper dataBaseHelper,final Affair oldAffair,final Affair newAffair){
+        SQLiteDatabase db = dataBaseHelper.getReadableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MoyuContract.AffairEntry.DESCRIPTION,newAffair.description);
+        contentValues.put(MoyuContract.AffairEntry.COMMENT,newAffair.comment);
+        contentValues.put(MoyuContract.AffairEntry.WEEK,newAffair.week);
+        contentValues.put(MoyuContract.AffairEntry.DAY_OF_WEEK,newAffair.day_of_week);
+        contentValues.put(MoyuContract.AffairEntry.DATE,newAffair.date);
+        contentValues.put(MoyuContract.AffairEntry.TIME,newAffair.time);
+        contentValues.put(MoyuContract.AffairEntry.REPEAT,newAffair.repeat);
+        contentValues.put(MoyuContract.AffairEntry.ALARM,newAffair.alarm);
+
+        String selection =  MoyuContract.AffairEntry.DESCRIPTION + " LIKE ? AND " +
+                MoyuContract.AffairEntry.COMMENT + " LIKE ? AND " +
+                MoyuContract.AffairEntry.WEEK + " LIKE ? AND " +
+                MoyuContract.AffairEntry.DAY_OF_WEEK + " LIKE ? AND " +
+                MoyuContract.AffairEntry.DATE + " LIKE ? AND " +
+                MoyuContract.AffairEntry.TIME + " LIKE ? AND " +
+                MoyuContract.AffairEntry.REPEAT + " LIKE ? AND " +
+                MoyuContract.AffairEntry.ALARM + " LIKE ?";
+        String[] selectionArgs = {oldAffair.description,
+                oldAffair.comment,
+                String.valueOf(oldAffair.week),
+                String.valueOf(oldAffair.day_of_week),
+                oldAffair.date,
+                oldAffair.time,
+                String.valueOf(oldAffair.repeat),
+                oldAffair.alarm};
+
+        int count = db.update(MoyuContract.AffairEntry.TABLE_NAME,contentValues,selection,selectionArgs);
+        return count;
+    }
+
+    public static int UpdateCourse(final DataBaseHelper dataBaseHelper,final Course oldCourse,final Course newCourse){
+        SQLiteDatabase db = dataBaseHelper.getReadableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MoyuContract.CourseEntry.COURSE_NAME,newCourse.course_name);
+        contentValues.put(MoyuContract.CourseEntry.CLASSROOM,newCourse.classroom);
+        contentValues.put(MoyuContract.CourseEntry.WEEK,newCourse.week);
+        contentValues.put(MoyuContract.CourseEntry.DAY_OF_WEEK,newCourse.day_of_week);
+        contentValues.put(MoyuContract.CourseEntry.DATE,newCourse.date);
+        contentValues.put(MoyuContract.CourseEntry.TIME,newCourse.time);
+
+        String selection = MoyuContract.CourseEntry.COURSE_NAME + " LIKE ? AND " +
+                MoyuContract.CourseEntry.CLASSROOM + " LIKE ? AND " +
+                MoyuContract.CourseEntry.WEEK + " LIKE ? AND " +
+                MoyuContract.CourseEntry.DAY_OF_WEEK + " LIKE ? AND " +
+                MoyuContract.CourseEntry.DATE + " LIKE ? AND " +
+                MoyuContract.CourseEntry.TIME + " LIKE ?";
+
+        String[] selectionArgs = {oldCourse.course_name,
+                oldCourse.classroom,
+                String.valueOf(oldCourse.week),
+                String.valueOf(oldCourse.day_of_week),
+                oldCourse.date,
+                oldCourse.time};
+
+        int count = db.update(MoyuContract.CourseEntry.TABLE_NAME,contentValues,selection,selectionArgs);
+        return count;
+    }
+
+    public static int UpdateDeletedRepeatAffair(final DataBaseHelper dataBaseHelper,final Affair oldAffair,final Affair newAffair){
+        SQLiteDatabase db = dataBaseHelper.getReadableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MoyuContract.DeletedRepeatAffairEntry.DESCRIPTION,newAffair.description);
+        contentValues.put(MoyuContract.DeletedRepeatAffairEntry.COMMENT,newAffair.comment);
+        contentValues.put(MoyuContract.DeletedRepeatAffairEntry.WEEK,newAffair.week);
+        contentValues.put(MoyuContract.DeletedRepeatAffairEntry.DAY_OF_WEEK,newAffair.day_of_week);
+        contentValues.put(MoyuContract.DeletedRepeatAffairEntry.DATE,newAffair.date);
+        contentValues.put(MoyuContract.DeletedRepeatAffairEntry.TIME,newAffair.time);
+
+        String selection =  MoyuContract.AffairEntry.DESCRIPTION + " LIKE ? AND " +
+                MoyuContract.AffairEntry.COMMENT + " LIKE ? AND " +
+                MoyuContract.AffairEntry.WEEK + " LIKE ? AND " +
+                MoyuContract.AffairEntry.DAY_OF_WEEK + " LIKE ? AND " +
+                MoyuContract.AffairEntry.DATE + " LIKE ? AND " +
+                MoyuContract.AffairEntry.TIME + " LIKE ?";
+
+        String[] selectionArgs = {oldAffair.description,
+                oldAffair.comment,
+                String.valueOf(oldAffair.week),
+                String.valueOf(oldAffair.day_of_week),
+                oldAffair.date,
+                oldAffair.time};
+
+        int count = db.update(MoyuContract.DeletedRepeatAffairEntry.TABLE_NAME,contentValues,selection,selectionArgs);
+        return count;
+    }
 }
